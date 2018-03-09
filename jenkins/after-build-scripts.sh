9ca39040ec70d0388d4cfbc80f60170c37773046
Luis Rosales <eng.luisrosales@gmail.com>
2018-03-09 13:05:38 +0100

    adding lint check

--- jenkins/after-build-scripts.sh
+++ jenkins/after-build-scripts.sh
@@ -8,3 +8,5 @@ echo "$BUILD_NAME"
 cd ../ci-concept-docker
 ./develop composer install
 ./develop t ./tests
+docker-compose exec -T php bash -c find ./src -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
+docker-compose exec -T php bash -c find ./tests -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
