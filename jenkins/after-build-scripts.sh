3f9d2fa42f45818aba8dea98547bab2b9418800d
Luis Rosales <eng.luisrosales@gmail.com>
2018-03-09 13:11:39 +0100

    adding phploc

--- jenkins/after-build-scripts.sh
+++ jenkins/after-build-scripts.sh
@@ -10,3 +10,4 @@ cd ../ci-concept-docker
 ./develop t ./tests
 docker-compose exec -T php bash -c find ./src -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
 docker-compose exec -T php bash -c find ./tests -name "*.php" -print0 | xargs -0 -n1 -P8 php -l
+docker-compose exec -T php bash -c './vendor/bin/phploc src'
