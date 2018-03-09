3237bdddf10a569605ba77c605c6ae5e03b54916
Luis Rosales <eng.luisrosales@gmail.com>
2018-03-09 13:06:17 +0100

    fixing the issue

--- src/ValidatePassword.php
+++ src/ValidatePassword.php
@@ -2,11 +2,11 @@
 
 class ValidatePassword {
 
-  const MIN_LENGTH = 6
+  const MIN_LENGTH = 6;
   const MAX_LENGTH = 20;
 
   public function validLength($pass) {
-    $passLength = strlen($pass
+    $passLength = strlen($pass);
     return $passLength >= self::MIN_LENGTH && $passLength <= self::MAX_LENGTH;
   }
 }
