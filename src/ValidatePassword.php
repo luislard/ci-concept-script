bcbc0bd351564d99444f99f64620e2e29be90341
Luis Rosales <eng.luisrosales@gmail.com>
2018-03-09 13:32:37 +0100

    fixing some issues

--- src/ValidatePassword.php
+++ src/ValidatePassword.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ *
+ * This class contains methods to Validate a Password
+ * @category Class
+ * @author Luis Rosales <eng.luisrosales@gmail.com>
+ */
 class ValidatePassword {
 
   const MIN_LENGTH = 6;
   const MAX_LENGTH = 20;
 
+  /**
+   *
+   * Test the given password and return true|false depending on criteria
+   * @param $pass string
+   */
   public function validLength($pass) {
     $passLength = strlen($pass);
     return $passLength >= self::MIN_LENGTH && $passLength <= self::MAX_LENGTH;
